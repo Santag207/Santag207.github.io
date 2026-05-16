f973ac41cc2123adc945f29cb07f18357629f832
Santag207 <castrozsantiago@javeriana.edu.co>
2026-05-15 23:08:11 -0500

    Cambios CV

--- javascript/cv-data.js
+++ javascript/cv-data.js
@@ -3,123 +3,123 @@ const cvData = {
     "C++": {
         "description": "Desarrollo de sistemas embebidos y algoritmos de alto rendimiento.",
         "projects": ["Controladores para drones", "Simulaciones físicas"],
-        "media": "https://raw.githubusercontent.com/devicons/devicon/master/icons/cplusplus/cplusplus-original.svg",
+        "media": "https://miro.medium.com/v2/resize:fit:1400/1*57VLS2QkegKDwZA4SlGWCg.gif",
         "type": "language"
     },
     "Python": {
         "description": "Automatización, análisis de datos y scripting para ingeniería.",
         "projects": ["Análisis de telemetría XAE", "Bots de automatización"],
-        "media": "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+        "media": "https://www.ideamotive.co/hs-fs/hubfs/Why%20Is%20Python%20a%20Great%20Choice%20For%20Data%20Analysis%3F%20-%20matplotgib.gif?width=576&height=384&name=Why%20Is%20Python%20a%20Great%20Choice%20For%20Data%20Analysis%3F%20-%20matplotgib.gif",
         "type": "language"
     },
     "MATLAB": {
         "description": "Cómputo numérico, procesamiento de señales y sistemas de control.",
         "projects": ["Diseño de filtros", "Simulación de órbitas"],
-        "media": "https://upload.wikimedia.org/wikipedia/commons/2/21/Matlab_Logo.png",
+        "media": "https://i.makeagif.com/media/12-10-2015/wMjZyo.gif",
         "type": "engineering"
     },
     "HTML5": {
         "description": "Maquetación web moderna y semántica.",
         "projects": ["Portfolio SANTAG-OS", "Dashboards interactivos"],
-        "media": "https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original.svg",
+        "media": "https://i0.wp.com/codemyui.com/wp-content/uploads/2015/07/material-design-register-login-form.gif?fit=690%2C345&ssl=1",
         "type": "language"
     },
     "CSS3": {
         "description": "Estilizado avanzado y animaciones complejas.",
         "projects": ["NASA Theme UI", "Animaciones de telemetría"],
-        "media": "https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original.svg",
+        "media": "https://devsnap.nyc3.digitaloceanspaces.com/devsnap.me/sam/tab-bar-menu-animation.gif",
         "type": "language"
     },
     "Dart": {
         "description": "Lenguaje para desarrollo de interfaces rápidas.",
         "projects": ["App móvil XAE"],
-        "media": "https://raw.githubusercontent.com/devicons/devicon/master/icons/dart/dart-original.svg",
+        "media": "https://flutterawesome.com/content/images/2019/05/app_running.gif",
         "type": "language"
     },
     "Kotlin": {
         "description": "Desarrollo de aplicaciones Android modernas.",
         "projects": ["Módulos de integración de sensores"],
-        "media": "https://raw.githubusercontent.com/devicons/devicon/master/icons/kotlin/kotlin-original.svg",
+        "media": "https://miro.medium.com/1*V1Z-O_yCf5KrrXTGJBpaAw.gif",
         "type": "language"
     },
     "JavaScript": {
         "description": "Lógica de cliente y SPA dinámicas.",
         "projects": ["Sistema de componentes SANTAG-OS"],
-        "media": "https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg",
+        "media": "https://namaste-javascript-handbook.vercel.app/assets/images/microtask4-934ae4c9468b589dad16549b47403751.gif",
         "type": "language"
     },
     "TypeScript": {
         "description": "JavaScript con tipado estático para robustez.",
         "projects": ["Refactorización de aplicaciones escalables"],
-        "media": "https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg",
+        "media": "https://user-images.githubusercontent.com/48876996/121569479-e179db80-ca31-11eb-8a48-9c3de9b142f3.gif",
         "type": "language"
     },
     "SQL": {
         "description": "Gestión y consulta de bases de datos relacionales.",
         "projects": ["Modelado de datos de misión"],
-        "media": "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original.svg",
+        "media": "https://miro.medium.com/1*KCIin6G-IrszDrhFZ6pb1g.gif",
         "type": "language"
     },
     "Java": {
         "description": "Desarrollo de aplicaciones robustas y backend.",
         "projects": ["Sistemas distribuidos"],
-        "media": "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+        "media": "https://mkpaz.github.io/atlantafx/assets/images/demo.gif",
         "type": "language"
     },
     "C#": {
         "description": "Desarrollo en .NET y lógica de videojuegos.",
         "projects": ["Scripts para Unity"],
-        "media": "https://raw.githubusercontent.com/devicons/devicon/master/icons/csharp/csharp-original.svg",
+        "media": "https://us1.discourse-cdn.com/flex022/uploads/ros/original/2X/c/ca5685068bc097ca8a3fa996b9809bbb5c346fc1.gif",
         "type": "language"
     },
 
     // IDEs
-    "Visual Studio Code": {
+    "VS Code": {
         "description": "Editor principal para desarrollo web y scripting.",
         "projects": ["Entorno de desarrollo diario"],
-        "media": "https://raw.githubusercontent.com/devicons/devicon/master/icons/vscode/vscode-original.svg",
+        "media": "https://developers.openai.com/images/codex/changelog/local_task.gif",
         "type": "ide"
     },
     "Qt Creator": {
         "description": "Desarrollo de interfaces gráficas para aplicaciones C++.",
         "projects": ["GUI de control de sensores"],
-        "media": "https://upload.wikimedia.org/wikipedia/commons/1/17/Qt_logo_2016.svg",
+        "media": "https://www.qt.io/hubfs/Qt%20Creator%20Iteration_sample.gif",
         "type": "ide"
     },
     "Android Studio": {
         "description": "Entorno oficial para desarrollo Android.",
         "projects": ["App móvil nativa"],
-        "media": "https://raw.githubusercontent.com/devicons/devicon/master/icons/android/android-original.svg",
+        "media": "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjlABhyphenhyphen1mHYL0vNFyHJDZHx10aSEiNckq56Id8dsX7-XFk2Dk4O4xcp8xUtd-CmKKp55rM4CESCjOCpcnJgYcKfDqVNfXEV1BYcwksV4MTeou0Y3-KAk3MrtixvZrAQSE5Yp8caUD9sfrI/s1600/image7.gif",
         "type": "ide"
     },
     "Arduino IDE": {
         "description": "Programación de microcontroladores.",
         "projects": ["Sistemas de adquisición de datos"],
-        "media": "https://raw.githubusercontent.com/devicons/devicon/master/icons/arduino/arduino-original.svg",
+        "media": "https://www.electronicwings.com/storage/PlatformSection/TopicContent/408/icon/Arduino%20PWM.gif",
         "type": "ide"
     },
     "PyCharm": {
         "description": "IDE especializado para desarrollo profesional en Python.",
         "projects": ["Análisis de datos avanzado", "Automatización industrial"],
-        "media": "https://raw.githubusercontent.com/devicons/devicon/master/icons/pycharm/pycharm-original.svg",
+        "media": "https://preview.redd.it/lets-plot-library-interactive-visualizations-in-pycharm-and-v0-w4xav2zei7n61.gif?width=960&auto=webp&s=c78b22fed23ac6e1201eb309d4f5ebfdd3a5f9a2",
         "type": "ide"
     },
     "IntelliJ IDEA": {
         "description": "IDE líder para desarrollo en Java y Kotlin.",
         "projects": ["Backend corporativo", "Integración de sistemas"],
-        "media": "https://raw.githubusercontent.com/devicons/devicon/master/icons/intellij/intellij-original.svg",
+        "media": "https://miro.medium.com/v2/resize:fit:1400/1*5J3TiknEN8hZQLtuJHR75Q.gif",
         "type": "ide"
     },
     "Eclipse": {
         "description": "Entorno de desarrollo clásico para Java.",
         "projects": ["Mantenimiento de sistemas legacy"],
-        "media": "https://raw.githubusercontent.com/devicons/devicon/master/icons/eclipse/eclipse-original.svg",
+        "media": "https://i.sstatic.net/CBTYr.gif",
         "type": "ide"
     },
     "Replit": {
         "description": "Plataforma de codificación colaborativa en la nube.",
         "projects": ["Prototipado rápido", "Scripting educativo"],
-        "media": "img/iconos/generic-tech.png",
+        "media": "https://global.discourse-cdn.com/streamlit/original/3X/f/7/f7136dad2cacb4ac9b92e8663ae15144b26bb328.gif",
         "type": "ide"
     },
 
@@ -127,61 +127,61 @@ const cvData = {
     "Flutter": {
         "description": "Desarrollo de aplicaciones móviles multiplataforma.",
         "projects": ["App de monitoreo de sensores", "Dashboard XAE"],
-        "media": "https://raw.githubusercontent.com/devicons/devicon/master/icons/flutter/flutter-original.svg",
+        "media": "https://miro.medium.com/1*KcxCTzISut_FEkKduDE4hw.gif",
         "type": "framework"
     },
     "Angular": {
         "description": "Framework para aplicaciones web escalables.",
         "projects": ["Paneles de control complejos"],
-        "media": "https://raw.githubusercontent.com/devicons/devicon/master/icons/angularjs/angularjs-original.svg",
+        "media": "https://miro.medium.com/v2/resize:fit:2000/1*t3P8xKodQ7Vr6FPeOoDA7w.gif",
         "type": "framework"
     },
     "Firebase": {
         "description": "Plataforma de desarrollo para apps móviles y web.",
         "projects": ["Autenticación y base de datos en tiempo real"],
-        "media": "https://raw.githubusercontent.com/devicons/devicon/master/icons/firebase/firebase-plain.svg",
+        "media": "https://miro.medium.com/v2/resize:fit:1400/1*HlLTnytZX9oZsg75-4mbtw.gif",
         "type": "tool"
     },
     "Git": {
         "description": "Control de versiones distribuido.",
         "projects": ["Gestión de repositorios colaborativos"],
-        "media": "https://raw.githubusercontent.com/devicons/devicon/master/icons/git/git-original.svg",
+        "media": "https://miro.medium.com/v2/resize:fit:1400/0*YcYoCIP1OJdx585y.gif",
         "type": "tool"
     },
     "GitHub": {
         "description": "Plataforma de alojamiento y colaboración de código.",
         "projects": ["Portafolio Open Source", "Gestión de proyectos XAE"],
-        "media": "https://raw.githubusercontent.com/devicons/devicon/master/icons/github/github-original.svg",
+        "media": "https://raw.githubusercontent.com/orhun/git-cliff/main/website/static/img/git-cliff-anim.gif  ",
         "type": "tool"
     },
     "Docker": {
         "description": "Contenerización de aplicaciones.",
         "projects": ["Despliegue de servicios micro-servicios"],
-        "media": "https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original.svg",
+        "media": "https://miro.medium.com/1*389AL3K_j8-b71PODN_1EA.gif",
         "type": "tool"
     },
     "Postman": {
         "description": "Plataforma para desarrollo y pruebas de APIs.",
         "projects": ["Validación de servicios backend"],
-        "media": "https://raw.githubusercontent.com/devicons/devicon/master/icons/postman/postman-original.svg",
+        "media": "https://blog.postman.com/wp-content/uploads/2024/04/debugging-gif-1.gif",
         "type": "tool"
     },
     "Power BI": {
         "description": "Análisis de negocios y visualización de datos.",
         "projects": ["Reportes de eficiencia operativa"],
-        "media": "https://upload.wikimedia.org/wikipedia/commons/c/cf/Power_BI_Logo.svg",
+        "media": "https://cdn.dribbble.com/userupload/42287106/file/original-3abb3e3a92cb3986e1c73cd0b8816ad1.gif",
         "type": "tool"
     },
     "R": {
         "description": "Lenguaje para computación estadística y gráficos.",
         "projects": ["Análisis de fallos estructurales"],
-        "media": "https://raw.githubusercontent.com/devicons/devicon/master/icons/r/r-original.svg",
+        "media": "https://1.bp.blogspot.com/-vEufhUJSdJ8/Uvy8gMgbVhI/AAAAAAAABaE/RunWCPWWNcA/s1600/animation1.gif",
         "type": "language"
     },
     "RStudio": {
         "description": "Entorno integrado para el lenguaje R.",
         "projects": ["Análisis estadístico avanzado"],
-        "media": "img/iconos/generic-tech.png",
+        "media": "https://www.datanovia.com/en/wp-content/uploads/dn-tutorials/r-tutorial/images/transition_time-faceted.gif",
         "type": "ide"
     },
 
@@ -189,43 +189,43 @@ const cvData = {
     "SolidWorks": {
         "description": "Diseño mecánico y modelado CAD 3D.",
         "projects": ["Estructura de mini-cohete", "Chasis para robot VEX"],
-        "media": "https://upload.wikimedia.org/wikipedia/commons/1/12/SolidWorks_Logo.svg",
+        "media": "https://czero-solutions.com/images/default/FEA/FEA-example_2.gif",
         "type": "engineering"
     },
     "ANSYS": {
         "description": "Simulación de ingeniería y análisis por elementos finitos.",
         "projects": ["Análisis estructural de alerones", "Simulación térmica"],
-        "media": "https://upload.wikimedia.org/wikipedia/commons/0/0e/ANSYS_logo.svg",
+        "media": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJNsBp6qwMIvp4okJXutQsPoJdKb8rhzh7zg&s",
         "type": "engineering"
     },
     "Autodesk Fusion": {
         "description": "Herramienta integrada de CAD, CAM y CAE.",
         "projects": ["Prototipado rápido", "Modelado de componentes"],
-        "media": "https://upload.wikimedia.org/wikipedia/commons/2/2c/Autodesk_Fusion_360_logo.svg",
+        "media": "https://fbi.cults3d.com/uploaders/20173526/illustration-file/09e7b643-73b2-4cce-a425-6f8ad0906b1f/Animation-Saturn-V.gif",
         "type": "engineering"
     },
     "Mastercam": {
         "description": "Software CAD/CAM para manufactura.",
         "projects": ["Programación de mecanizado CNC"],
-        "media": "img/iconos/generic-tech.png",
+        "media": "https://static.wixstatic.com/media/4d1d08_c3019d2f693e4111b7453b4c6da3f9e1~mv2.gif",
         "type": "engineering"
     },
     "CFD": {
         "description": "Mecánica de fluidos computacional para análisis aerodinámico.",
         "projects": ["Optimización de perfiles alares", "Flujo en toberas"],
-        "media": "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExMXl2NmN6Z2Z6Z2Z6Z2Z6Z2Z6Z2Z6Z2Z6Z2Z6Z2Z6Z2Z6Z2Z6Z2Z6Jm09MQ/CFD_Sim.gif",
+        "media": "https://i.makeagif.com/media/4-07-2022/4Ao1mC.gif",
         "type": "engineering"
     },
     "Unity": {
         "description": "Desarrollo de entornos virtuales y simulaciones interactivas.",
         "projects": ["Simulador de vuelo VR", "Entorno de entrenamiento robótico"],
-        "media": "https://raw.githubusercontent.com/devicons/devicon/master/icons/unity/unity-original.svg",
+        "media": "https://us1.discourse-cdn.com/flex022/uploads/ros/original/2X/c/ca5685068bc097ca8a3fa996b9809bbb5c346fc1.gif",
         "type": "game_dev"
     },
     "Linux": {
         "description": "Sistema operativo para desarrollo y servidores.",
         "projects": ["Configuración de estaciones de trabajo"],
-        "media": "https://raw.githubusercontent.com/devicons/devicon/master/icons/linux/linux-original.svg",
+        "media": "https://user-images.githubusercontent.com/24392180/77252333-35997400-6c64-11ea-9627-bb56ee14ae22.gif",
         "type": "os"
     },
 
@@ -233,43 +233,43 @@ const cvData = {
     "Aerospace Engineering": {
         "description": "Diseño y desarrollo de aeronaves y naves espaciales.",
         "projects": ["Mini-lanzadores suborbitales", "Cargas útiles"],
-        "media": "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExMXl2NmN6Z2Z6Z2Z6Z2Z6Z2Z6Z2Z6Z2Z6Z2Z6Z2Z6Z2Z6Z2Z6Z2Z6Jm09MQ/Aerospace.gif",
+        "media": "https://d18l82el6cdm1i.cloudfront.net/image_optimizer/06bf29cd018817abeb0b0fdef33e914ed0b15095.gif",
         "type": "interest"
     },
     "Rocket Science": {
         "description": "Propulsión, balística y dinámica de vuelo orbital.",
         "projects": ["Modelado de motores de combustible sólido"],
-        "media": "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExMXl2NmN6Z2Z6Z2Z6Z2Z6Z2Z6Z2Z6Z2Z6Z2Z6Z2Z6Z2Z6Z2Z6Z2Z6Jm09MQ/Rocketry.gif",
+        "media": "https://i.makeagif.com/media/10-14-2019/rLeCD6.gif",
         "type": "interest"
     },
     "Avionics Systems": {
         "description": "Sistemas electrónicos integrados en plataformas aeroespaciales.",
         "projects": ["Sistemas de telemetría en tiempo real"],
-        "media": "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExMXl2NmN6Z2Z6Z2Z6Z2Z6Z2Z6Z2Z6Z2Z6Z2Z6Z2Z6Z2Z6Z2Z6Z2Z6Jm09MQ/Avionics.gif",
+        "media": "https://www.avmap.it/upload/Avionics/EkpV-functions-anim.gif",
         "type": "interest"
     },
     "Sensor Integration": {
         "description": "Fusión de datos y acoplamiento de hardware sensorial.",
         "projects": ["IMU/GPS Kalman Filtering"],
-        "media": "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExMXl2NmN6Z2Z6Z2Z6Z2Z6Z2Z6Z2Z6Z2Z6Z2Z6Z2Z6Z2Z6Z2Z6Z2Z6Jm09MQ/Sensors.gif",
+        "media": "https://pressbooks-dev.oer.hawaii.edu/epet302/wp-content/uploads/sites/147/2020/12/unnamed.gif",
         "type": "interest"
     },
     "Control Systems": {
         "description": "Teoría de control y retroalimentación para estabilidad.",
         "projects": ["Control PID para estabilización de empuje"],
-        "media": "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExMXl2NmN6Z2Z6Z2Z6Z2Z6Z2Z6Z2Z6Z2Z6Z2Z6Z2Z6Z2Z6Z2Z6Z2Z6Jm09MQ/Control.gif",
+        "media": "https://upload.wikimedia.org/wikipedia/commons/b/b8/ControlSurfaces.gif",
         "type": "interest"
     },
     "Computational Physics": {
         "description": "Uso de métodos numéricos para resolver problemas físicos.",
         "projects": ["Simulación de sistemas de partículas", "Modelado de transferencia de calor"],
-        "media": "img/iconos/generic-tech.png",
+        "media": "https://lh7-rt.googleusercontent.com/docsz/AD_4nXcXy6ziUJB7TSfaeQp1gC0Z-hUbA1TFqh1EVApc74W8sBx3usfoSNQJKSxrsw3g54CFIjOvLJJEkI0eDWsBmpqyZsfybcXemBvG9PlhfftFFzetKLqdKHm3rtzpsFhRPDViusfG9Q?key=GsZtLR7HovoTcpNoVaR1TXFk",
         "type": "interest"
     },
     "Data Science": {
         "description": "Extracción de conocimiento a partir de grandes conjuntos de datos.",
         "projects": ["Predicción de trayectorias", "Clasificación de señales"],
-        "media": "img/iconos/generic-tech.png",
+        "media": "https://i.pinimg.com/originals/f9/a8/73/f9a8736c48cdaf40d3b9c1721c6f5161.gif",
         "type": "interest"
     }
 };
